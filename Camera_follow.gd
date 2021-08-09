c7ba404c32750697a2640655b6be3de358c3712d
M3CG <cgmario1999@gmail.com>
2021-08-09 18:22:13 -0300

    modificaciones al codig original

--- Camera_follow.gd
+++ Camera_follow.gd
@@ -1,23 +1,11 @@
 extends Camera
 
-var distance = 40
-var height = 2
-
+var velocidad_de_suavizado = 5
+var lejania = 30
+var altitud = 10
 func _ready():
-	
-	set_physics_process(true)
 	set_as_toplevel(true)
-
+	
 func _physics_process(delta):
-	var target = get_parent().get_global_transform().origin
-	var pos = get_global_transform().origin
-	var up = Vector3.UP
 	
-	var offset = pos - target
-	
-	offset = offset.normalized() * distance
-	offset.y = height
-	
-	pos = target + offset
-	
-	look_at_from_position(pos,target,up)
+	self.translation = lerp(self.translation,get_parent().translation + Vector3(0,altitud,lejania), velocidad_de_suavizado * delta)
